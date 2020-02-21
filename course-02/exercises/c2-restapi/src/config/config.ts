b6927bb5593c5d860caa02e98c296cf72467273c
Zubair Riaz <zubairriaz78696@gmail.com>
2020-02-22 02:19:12 +0500

    changing name of folders

---
+++ course-02/exercises/c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramdev",
+    "password": "12345678",
+    "database": "udagramdev",
+    "host": "udagramdev.cjplaq49jeug.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-zubairr-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
